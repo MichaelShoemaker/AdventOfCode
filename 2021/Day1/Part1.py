1dea43d120bd69304a05fe2e8d932bbb693b0fbf
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2021-12-01 05:59:38 -0600

    Defined Day1 Part1 Function

--- 2021/Day1/Part1.py
+++ 2021/Day1/Part1.py
@@ -1,4 +1,15 @@
 def count_increases(file):
     f = open(file)
     data = f.readlines()
-    current_line = 
+    count = 0
+    previous_line = -9999999
+
+    for line in data:
+        current_line = line
+        if int(current_line) > int(previous_line):
+            count +=1
+        previous_line = current_line
+    return count
+
+if __name__=='__main__':
+    print(count_increases('input.txt'))
