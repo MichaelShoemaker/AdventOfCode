3ff106bdfa89ec44e87b9b18850f404b1441e825
MichaelShoemaker <mikeshoemaker2000@yahoo.com>
2021-12-08 14:38:29 -0600

    Still working on Day8 Part2

--- 2021/Day8/Part2.py
+++ 2021/Day8/Part2.py
@@ -17,20 +17,34 @@ def parse_text(file):
         all_lines.append(([set(x) for x in data[0].split(' ')],[set(x) for x in data[1].split(' ')]))
         
     for a in all_lines:
+        ciph = {}
         for i in a[1]:
-            if len(i) == 7:
-                print(8,end="")
+            if len(i) == 2:
+                ciph[1] = i
+                #print(1, end = "")
             elif len(i) == 4:
-                print(4,end="")
-            elif len(i) == 2:
-                print(1, end = "")
-            elif len(i) == 3:
-                print(7,end ="")
-            elif len(i) == 6:
-                print(0,end ="")
-            else:
-                print(i,end="")
-                
+                ciph[4] = i
+                #print(4,end="") 
+        for i in a[1]:
+            if len(i) == 5:
+                if len(i.intersection(ciph[4])) == 3: 
+                    ciph[5] = i 
+                elif len(i.intersection(ciph[4])) == 2:
+                else:
+                    ciph[2] = i
+        print(ciph)
+            # elif len(i) == 7:
+            #     ciph[8] = i
+            #     print(8,end="")
+
+            
+            # elif len(i) == 3:
+            #     print(7,end ="")
+            # elif len(i) == 6:
+            #     print(0,end ="")
+            # else:
+            #     print(i,end="")
+
         print()
 
 
